cc23894db37680af920210bef3ce110a069136a0
Diego Garza <86547241+diego-gt@users.noreply.github.com>
2021-10-24 17:13:04 -0500

    Set grid dimensions to integers
    The amout of cells in the grid will be width*height.

--- include/TextGrid.hpp
+++ include/TextGrid.hpp
@@ -8,11 +8,11 @@
 class USC_DLL_EXPORT TextGrid {
 public:
   TextGrid() = default;
-  TextGrid(double height, double width);
-  void Draw(bool print_corners);
+  TextGrid(int height, int width);
+  void Draw();
 
 private:
   // Grid dimensions (number of cells)
-  double m_height{0}, m_width{0};
+  int m_height{0}, m_width{0};
   std::vector<std::vector<TextCell>> m_cells;
 };
--- src/TextGrid.cpp
+++ src/TextGrid.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <string>
 
-TextGrid::TextGrid(double height, double width) {
+TextGrid::TextGrid(int height, int width) {
   m_height = height;
   m_width  = width;
 }
