0f3a408366d958bfbdb17361518743e67db69924
Diego Garza <diego.gt.rh@gmail.com>
2021-10-28 09:26:43 -0500

    Update demo to show ResizeCells() and SetCell()

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,13 @@ int main() {
   TextGrid grid(5, 5);
 
   grid.InitializeCells('x');
-  // grid.Debug();
+  grid.ResizeCells(2);
+  grid.Draw();
+  grid.SetCell(1, 1, 'a');
+  grid.SetCell(2, 2, 'b');
+  grid.SetCell(1, 2, 'c');
+  grid.SetCell(3, 3, 'd');
+  grid.SetCell(5, 1, 'e');
   grid.Draw();
 
   return 0;
