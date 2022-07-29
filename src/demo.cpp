76ab81e934b1bed5ed2c0761775160a7f90e8e57
Diego Garza <diego.gt.rh@gmail.com>
2022-07-29 17:18:14 -0500

    Use unicode borders in demo

--- src/demo.cpp
+++ src/demo.cpp
@@ -5,28 +5,28 @@
 int main() {
     Cell c("hello");
     std::cout << "Cell demo:\n";
-    std::cout << "left_border";
+    std::cout << text::borders.vertical.unicode;
     c.Display();
-    std::cout << "right_border\n";
+    std::cout << text::borders.vertical.unicode << '\n';
 
     Cell d("hello", 10, text::Left);
     std::cout << "Cell demo:\n";
-    std::cout << "left_border";
+    std::cout << text::borders.vertical.unicode;
     d.Display();
-    std::cout << "right_border\n";
+    std::cout << text::borders.vertical.unicode << '\n';
 
     d.Realign(text::Right);
     std::cout << "Cell demo:\n";
-    std::cout << "left_border";
+    std::cout << text::borders.vertical.unicode;
     d.Display();
-    std::cout << "right_border\n";
+    std::cout << text::borders.vertical.unicode << '\n';
 
     d.AdjustWidth(20);
     d.Realign(text::Center);
     std::cout << "Cell demo:\n";
-    std::cout << "left_border";
+    std::cout << text::borders.vertical.unicode;
     d.Display();
-    std::cout << "right_border\n";
+    std::cout << text::borders.vertical.unicode << '\n';
 
     return 0;
 }
